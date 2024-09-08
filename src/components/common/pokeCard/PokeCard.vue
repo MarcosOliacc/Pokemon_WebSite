843e63b7fa9515d4e9b89d7da0e3563030016902
Marcos Vinicius <marcosoliacc@gmail.com>
2024-09-08 13:11:07 -0300

    atualização em podium sect

---
+++ src/components/common/pokeCard/PokeCard.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+<template>
+    <div class="conteiner">
+        <div class="imgContent">
+            <img class="pokeImg" v-bind:src="'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/015.png'" alt="">
+        </div>
+        <div class="description">
+            
+        </div>
+    </div>
+</template>
