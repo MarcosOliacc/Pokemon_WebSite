9f56009771d2a5b9a991fc4a0656b759a4f1ee80
Marcos Vinicius <marcosoliacc@gmail.com>
2024-09-08 13:41:00 -0300

    Update HomeView.vue

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,12 +1,11 @@
 <script setup>
 import WhoIsPoke from '@/components/whoIsPoke/WhoIsPoke.vue';
-import PokeCard from '@/components/common/pokeCard/pokeCard.vue';
 </script>
 
 <template>
   <main>
     <WhoIsPoke/>
-    <PokeCard/>
+
     <h1>esta é a Home Page</h1>
   </main>
 </template>
